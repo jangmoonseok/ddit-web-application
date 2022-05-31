d13716a55e77cbbdd816b6c2cadc3137b403431c
DESKTOP-BP73NMV\PC-25 <cacao621@naver.com>
2022-05-31 17:34:41 +0900

    boardList

---
+++ WebContent/WEB-INF/views/board/regist_success.jsp
@@ -0,0 +1,8 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<script>
+	alert("글 등록에 성공하였습니다.");
+	
+	window.close();
+	window.opener.location.reload();
+</script>
---
+++ src/com/jsp/dto/BoardVO.java
@@ -0,0 +1,53 @@
+package com.jsp.dto;
+
+import java.util.Date;
+
+public class BoardVO {
+
+	private int bno;
+	private String title;
+	private String writer;
+	private String content;
+	private Date regDate = new Date();
+	private int viewcnt;
+	
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regdate) {
+		this.regDate = regdate;
+	}
+	public int getViewcnt() {
+		return viewcnt;
+	}
+	public void setViewcnt(int viewcnt) {
+		this.viewcnt = viewcnt;
+	}
+
+	
+	
+}
